2a74fb4b17ae1fc1e5f359cb916add35facea60d
teppohalme <teppo.halme@myy.haaga-helia.fi>
2025-09-15 12:42:31 +0300

    tehtävä 3.3.

--- src/main/java/hh/be/bookstore/web/BookController.java
+++ src/main/java/hh/be/bookstore/web/BookController.java
@@ -1,10 +1,13 @@
 package hh.be.bookstore.web;
 
+import hh.be.bookstore.domain.Book;
 import hh.be.bookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -17,4 +20,21 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist"; 
     }
-}
+
+    @GetMapping("/add")
+    public String showAddBookForm(Book book) {
+        return "addbook";
+    }
+
+    @PostMapping("/add")
+    public String addBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:/booklist";
+    }
+}   
